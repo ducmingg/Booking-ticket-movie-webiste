3569d5935d2af3926d472c5de150d38b6e0d082b
duckies <voducminh39@gmail.com>
2024-03-29 00:17:47 +0700

    feature/#3-add ticket box

---
+++ js/book_moive.js
@@ -0,0 +1,88 @@
+const enable__shtime = document.querySelectorAll('.shtime__item');
+
+enable__shtime.forEach(item => {
+
+    item.addEventListener("click", () => {
+        enable__shtime.forEach(element => {
+            if (element.classList.contains('enable')) {
+                element.classList.remove('enable');
+            }
+        });
+        item.classList.toggle('enable');
+    });
+});
+
+//
+
+const minus_f = document.querySelector('.minus_f');
+const minus_s = document.querySelector('.minus_s');
+const minus_t = document.querySelector('.minus_t');
+const count_f = document.querySelector('.count_f');
+const count_s = document.querySelector('.count_s');
+const count_t = document.querySelector('.count_t');
+const plus_f = document.querySelector('.plus_f');
+const plus_s = document.querySelector('.plus_s');
+const plus_t = document.querySelector('.plus_t');
+
+// let cf, cs, ct = 0;
+// minus_f.addEventListener("click", () => {
+//     let isCount = parseInt(count_f.textContent);
+//     // count_f.innerHTML = isCount + 1;
+//     count_f.innerHTML =cf+1; 
+// })
+
+let cf = 0;
+let ct = 0;
+let cs = 0;
+minus_f.addEventListener("click", () => {
+    let isCount = parseInt(count_f.textContent)
+    if (isCount >= 1) {
+        isCount--; // Tăng giá trị của cf lên 1
+        count_f.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+});
+minus_s.addEventListener("click", () => {
+    let isCount = parseInt(count_s.textContent)
+    if (isCount >= 1) {
+        isCount--; // Tăng giá trị của cf lên 1
+        count_s.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+}); minus_t.addEventListener("click", () => {
+    let isCount = parseInt(count_t.textContent)
+    if (isCount >= 1) {
+        isCount--; // Tăng giá trị của cf lên 1
+        count_t.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+});
+
+
+plus_f.addEventListener('click', () => {
+    let isCount = parseInt(count_f.textContent)
+    if (isCount <= 7) {
+        isCount++; // Tăng giá trị của cf lên 1
+        count_f.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+})
+
+
+plus_s.addEventListener('click', () => {
+    let isCount = parseInt(count_s.textContent)
+    if (isCount <= 7) {
+        isCount++; // Tăng giá trị của cf lên 1
+        count_s.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+})
+
+
+
+
+plus_t.addEventListener('click', () => {
+    let isCount = parseInt(count_t.textContent)
+    if (isCount <= 7) {
+        isCount++; // Tăng giá trị của cf lên 1
+        count_t.innerHTML = isCount; // Gán giá trị mới của cf vào textContent của count_f
+    }
+})
+
+
+
